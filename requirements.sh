5630fc2062c4bd63e8486cc47df4abb1ea32fba8
Juan Pizarro <jpizarrom@gmail.com>
2017-06-13 12:16:58 -0400

    remove pandas

--- requirements.sh
+++ requirements.sh
@@ -5,5 +5,4 @@ pip install --upgrade pip wheel
 pip install --use-wheel --no-binary numpy numpy==1.11.1
 pip install --use-wheel --no-binary scipy scipy==0.18.1
 pip install --use-wheel scikit-learn==0.18
-pip install --use-wheel pandas==0.18.1
 pip install --use-wheel joblib==0.11
