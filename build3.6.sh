eca3e5720f7178f832c4f85cdec3b4c41d4dccc8
Juan Pizarro <jpizarrom@gmail.com>
2017-09-05 15:58:21 -0300

    py36

---
+++ build3.6.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -ex
+
+do_pip () {
+    /outputs/requirements.sh
+}
+
+strip_virtualenv () {
+    echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
+    find $VIRTUAL_ENV/lib64/python3.6/site-packages/ -name "*.so" | grep -v /PIL/ | xargs strip
+    echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
+
+    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/ && zip -r -9 -q /outputs/venv.zip joblib* ; popd
+
+    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
+    echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
+
+    pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
+    echo "venv compressed size $(du -sh /outputs/full-venv.zip | cut -f1)"
+}
+
+shared_libs () {
+    libdir="$VIRTUAL_ENV/lib64/python3.6/site-packages/lib/"
+    mkdir -p $VIRTUAL_ENV/lib64/python3.6/site-packages/lib || true
+    cp /usr/lib64/atlas/* $libdir
+    cp /usr/lib64/libquadmath.so.0 $libdir
+    cp /usr/lib64/libgfortran.so.3 $libdir
+}
+
+uploadtos3 () {
+    pip install awscli
+    aws s3 cp /outputs/venv.zip s3://$S3_BUCKET/"$S3_PREFIX"venv.`date +%Y%m%d%H%M%S`.zip
+}
+
+main () {
+    /usr/bin/python3.6 -m venv --copies /sklearn_build
+    source /sklearn_build/bin/activate
+
+    do_pip
+
+    shared_libs
+
+    strip_virtualenv
+
+    uploadtos3
+}
+main
--- requirements.sh
+++ requirements.sh
@@ -4,5 +4,10 @@ set -ex
 pip install --upgrade pip wheel
 pip install --use-wheel --no-binary numpy numpy==1.11.1
 pip install --use-wheel --no-binary scipy scipy==0.18.1
-pip install --use-wheel scikit-learn==0.18
 pip install --use-wheel joblib==0.11
+
+pip install --use-wheel pillow==4.2.1
+pip install --use-wheel scikit-image==0.12.3
+pip install --use-wheel image-match==1.1.2
+pip install --use-wheel imagehash==3.1
+pip install --use-wheel psycopg2==2.7.3.1
